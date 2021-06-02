df0d4b12401fa35d910554dd762e70daf2ce0e74
Mahas Milhar <mahasmm787@gmail.com>
2021-06-02 21:07:58 +0530

    while loop

--- Array2D.java
+++ Array2D.java
@@ -3,13 +3,12 @@ public class Array2D {
     public static void main(String[] args) {
         int[][] Poly = { { 1, 2, 3, 4 }, { 3, 4, 5, 6 }, { 2, 3, 4, 5 } };
         // 1*X^3+2*X^4+...
-
-        for (int i = 0; i < Poly.length; i++) {
-            for (int j = 0; j < Poly[i].length; j++) {
-                System.out.print(Poly[i][j]);
-            }
-            System.out.println("");
-        }
+        int i=0;
+      while (i < 10 )
+      {
+          System.out.println(i);
+          i++;
+      }
     }
 }
 // Hi my name is Mahas
